0a75d4d6088f402e7a527f07b20aacc2891c6909
RJ Jacobs <rjjacobs200@gmail.com>
2019-02-01 16:17:52 -0600

    now just have to tweak the bin search for non-members

--- my_binary_search.rb
+++ my_binary_search.rb
@@ -1,33 +1,37 @@
 
-include 'Random'
-
 class Array
 
-  def my_binary_search element, range = (0..self.length)
+  def my_binary_search element, range = (0..(self.length - 1))
 
-    front, back, mid = range.begin, range.end, (back - front) / 2 + front
+    return nil if range.size == 1
 
-    case self[mid] <=> element
+    left, right, = range.begin, range.end
+    mid = ( (right - left) / 2 + left )
+
+    case element <=> self[mid]
     when -1
-      return false if low >= mid
-      return my_binary_search element, front..(mid - 1)
+      my_binary_search element, left..(mid - 1)
     when 0
       puts 'ladies and gentlemen, we got em'
-      return mid
+      mid
     when 1
-      return false if high <= mid
-      return my_binary_search element, (mid + 1)..back
+      my_binary_search element, (mid + 1)..right
     else
       puts "goodness me, something's gone terribly wrong!"
-      return nil
+      nil
     end
 
   end
 
 end
 
-data = Array.new 50 do |param|
-  Random.rand 10000
+data = Array.new 100 do |param|
+  Random.rand 1000
 end
+data.sort!
 
-puts data
+index_to_find = Random.rand data.length
+# element_to_find = data[index_to_find]
+element_to_find = Random.rand 1000
+
+puts data.my_binary_search element_to_find
