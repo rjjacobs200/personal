a0adc0df3143bfd20c4d89ab3dedea974601784d
rj <robertj2@illinois.edu>
2019-02-15 19:38:54 -0600

    ineffib rust

---
+++ inef_fib.rs
@@ -0,0 +1,13 @@
+
+const BENCHMARKING_INDEX: u64 = 40;
+
+use std::io;
+
+fn inef_fib(n: u64) -> u64 {
+    if n <= 1 {return n};
+    inef_fib(n - 1) + inef_fib(n - 2)
+}
+
+fn main() {
+    println!("fib({}): {}", BENCHMARKING_INDEX, inef_fib(BENCHMARKING_INDEX));
+}
