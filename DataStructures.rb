6729915bc7a04deab6f6ba0c43801dded9d2932b
rj <robertj2@illinois.edu>
2019-03-01 11:38:02 -0600

    some more btree functions, tester

--- DataStructures.rb
+++ DataStructures.rb
@@ -3,19 +3,10 @@ class Tree
     def initialize data, *children
       @data, @children = data, children
     end
-    def each
-      if @left  then @left.each  {|i| yield i} end
-      yield @data
-      if @right then @right.each {|i| yield i} end
-    end
   end
   def initialize
     @root = nil
   end
-  def each
-    @root.each {|i| yield i}
-  end
-  alias iterate each
 end
 
 class BinaryTree
@@ -28,6 +19,23 @@ class BinaryTree
       yield @data
       if @right then @right.each {|i| yield i} end
     end
+    def height
+      [if @left  then @left.height  else 0 end,
+       if @right then @right.height else 0 end].max
+    end
+    def balance_factor
+      @right.height - @left.height
+    end
+  end
+  def each
+    @root.each {|i| yield i}
+  end
+  alias iterate each
+  def height
+    @root.height
+  end
+  def root_balance_factor
+    @root.balance_factor
   end
 end
 
--- DataStructures_test.rb
+++ DataStructures_test.rb
@@ -2,3 +2,11 @@ STRUCTURE_SIZE  = ARGV[0].to_i
 MAXIMUM_ELEMENT = STRUCTURE_SIZE * 100
 
 require_relative 'DataStructures.rb'
+require 'benchmark'
+
+bst = BinarySearchTree.new
+puts "For BinarySearchTree of size: #{STRUCTURE_SIZE}"
+Benchmark.bm(7) do |x|
+  x.report("add:") {(0..STRUCTURE_SIZE).each {bst.add(Random.rand MAXIMUM_ELEMENT)}}
+end
+puts bst.root_balance_factor
