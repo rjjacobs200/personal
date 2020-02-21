f5f412e489a072334ac27fb0e97cf58688160fe7
RJ Jacobs <rjjacobs200@gmail.com>
2020-02-21 16:08:37 -0600

    retrying the commit

--- ruby_qt_test.rb
+++ ruby_qt_test.rb
@@ -1,37 +1,47 @@
+
+WIDTH  = 350
+HEIGHT = 250 
+
 require 'Qt'
 
-
-class QtApp < Qt::Widget
+class MyQtApp < Qt::Widget
 
     def initialize
         super
-        
-        setWindowTitle "Buttons"
-        
+        setWindowTitle "My Window"
         init_ui
-        
-        resize 330, 170
-        move 300, 300
-
+        resize WIDTH, HEIGHT
+        center
         show
     end
     
-    def init_ui
-        vbox = Qt::VBoxLayout.new self
-        hbox = Qt::HBoxLayout.new
-
-        ok = Qt::PushButton.new "OK", self
-        apply = Qt::PushButton.new "Apply", self
-
-        hbox.addWidget ok, 1, Qt::AlignRight
-        hbox.addWidget apply
-
-        vbox.addStretch 1
-        vbox.addLayout hbox
+    def center
+        qdw = Qt::DesktopWidget.new
+        x = 0.5 * (qdw.width  - WIDTH)
+        y = 0.5 * (qdw.height - HEIGHT)
+        move x, y
     end
     
+    def init_quit_btn
+        quit_btn = Qt::PushButton.new "Quit", self
+        quit_btn.resize 80, 30
+        quit_btn.move WIDTH - 90, HEIGHT - 40
+        connect quit_btn, SIGNAL('clicked()'), $qApp, SLOT('quit()')
+    end
+    
+    def init_ok_btn
+        ok_btn = Qt::PushButton.new "OK", self
+        ok_btn.resize 80, 30
+        ok_btn.move WIDTH - 180, HEIGHT - 40
+    end
+    
+    def init_ui
+        init_quit_btn
+        init_ok_btn
+    end
+        
 end
 
 app = Qt::Application.new ARGV
-QtApp.new
+MyQtApp.new
 app.exec
