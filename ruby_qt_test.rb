f5006054e54c0892487c95608c541ed64de577e9
RJ Jacobs <rjjacobs200@gmail.com>
2020-02-21 15:41:42 -0600

    added stuff to ruby_qt_test, got rid of term.c

--- ruby_qt_test.rb
+++ ruby_qt_test.rb
@@ -1,6 +1,6 @@
 
-WIDTH  = 250
-HEIGHT = 150 
+WIDTH  = 350
+HEIGHT = 250 
 
 require 'Qt'
 
@@ -22,11 +22,22 @@ class MyQtApp < Qt::Widget
         move x, y
     end
     
-    def init_ui
+    def init_quit_btn
         quit_btn = Qt::PushButton.new "Quit", self
         quit_btn.resize 80, 30
         quit_btn.move WIDTH - 90, HEIGHT - 40
         connect quit_btn, SIGNAL('clicked()'), $qApp, SLOT('quit()')
+    end
+    
+    def init_ok_btn
+        ok_btn = Qt::PushButton.new "OK", self
+        ok_btn.resize 80, 30
+        o.move WIDTH - 180, HEIGHT - 40
+    end
+    
+    def init_ui
+        init_quit_btn
+        init_ok_btn
     end
         
 end
