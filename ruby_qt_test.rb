05aa87acaa9d31b1bcd1275bddf42f2f862deca5
RJ Jacobs <rjjacobs200@gmail.com>
2020-02-21 16:23:38 -0600

    for some reason the changes got releted?

--- ruby_qt_test.rb
+++ ruby_qt_test.rb
@@ -1,47 +1,37 @@
-
-WIDTH  = 350
-HEIGHT = 250 
-
 require 'Qt'
 
-class MyQtApp < Qt::Widget
+
+class QtApp < Qt::Widget
 
     def initialize
         super
-        setWindowTitle "My Window"
+        
+        setWindowTitle "Buttons"
+        
         init_ui
-        resize WIDTH, HEIGHT
-        center
+        
+        resize 330, 170
+        move 300, 300
+
         show
     end
     
-    def center
-        qdw = Qt::DesktopWidget.new
-        x = 0.5 * (qdw.width  - WIDTH)
-        y = 0.5 * (qdw.height - HEIGHT)
-        move x, y
-    end
-    
-    def init_quit_btn
-        quit_btn = Qt::PushButton.new "Quit", self
-        quit_btn.resize 80, 30
-        quit_btn.move WIDTH - 90, HEIGHT - 40
-        connect quit_btn, SIGNAL('clicked()'), $qApp, SLOT('quit()')
-    end
-    
-    def init_ok_btn
-        ok_btn = Qt::PushButton.new "OK", self
-        ok_btn.resize 80, 30
-        ok_btn.move WIDTH - 180, HEIGHT - 40
-    end
-    
     def init_ui
-        init_quit_btn
-        init_ok_btn
+        vbox = Qt::VBoxLayout.new self
+        hbox = Qt::HBoxLayout.new
+
+        ok = Qt::PushButton.new "OK", self
+        apply = Qt::PushButton.new "Apply", self
+
+        hbox.addWidget ok, 1, Qt::AlignRight
+        hbox.addWidget apply
+
+        vbox.addStretch 1
+        vbox.addLayout hbox
     end
-        
+    
 end
 
 app = Qt::Application.new ARGV
-MyQtApp.new
+QtApp.new
 app.exec
