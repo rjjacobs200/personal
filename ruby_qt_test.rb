5b49d8d4fc6ae3cd943d6881ce0a1d31d9108cdb
RJ Jacobs <rjjacobs200@gmail.com>
2020-02-22 13:22:42 -0600

    now gitnoring binaries

--- ruby_qt_test.rb
+++ ruby_qt_test.rb
@@ -1,15 +1,11 @@
 require 'Qt'
 
-
 class QtApp < Qt::Widget
 
     def initialize
         super
-        
         setWindowTitle "Buttons"
-        
         init_ui
-        
         resize 330, 170
         move 300, 300
 
