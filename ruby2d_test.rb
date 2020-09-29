12507f4d48a0c75c57bcd96c17b4362bc43f9f91
RJ Jacobs <rjjacobs200@gmail.com>
2020-09-29 15:30:29 -0500

    testing different ruby2d window features

--- ruby2d_test.rb
+++ ruby2d_test.rb
@@ -1,13 +1,12 @@
 require 'ruby2d'
 
-# Set the size of the application window
-set width: 300, height: 200
+#Window Title
+set title: "My test window"
 
-#Make a square I guess?
-square = Square.new
+#Testing out the background color
+set background: 'navy'
 
-#Color that square
-square.color = 'red'
+Triangle.new x1: 320, y1: 50, x2: 540, y2: 430, x3: 100, y3: 430, color: ['red', 'green', 'blue']
 
 #Display the window
 show
