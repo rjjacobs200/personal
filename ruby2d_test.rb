1d35bc53f165487a47fc20d26fc74eaad103b1c9
RJ Jacobs <rjjacobs200@gmail.com>
2020-09-29 15:16:21 -0500

    first ruby2d test iteration

---
+++ ruby2d_test.rb
@@ -0,0 +1,13 @@
+require 'ruby2d'
+
+# Set the size of the application window
+set width: 300, height: 200
+
+#Make a square I guess?
+square = Square.new
+
+#Color that square
+square.color = 'red'
+
+#Display the window
+show
