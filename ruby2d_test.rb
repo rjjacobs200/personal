3d47839affbea8cb19d5967b51aadc89f6ac750f
RJ Jacobs <rjjacobs200@gmail.com>
2020-09-29 15:34:03 -0500

    ruby2d going borderless

--- ruby2d_test.rb
+++ ruby2d_test.rb
@@ -6,7 +6,11 @@ set title: "My test window"
 #Testing out the background color
 set background: 'navy'
 
+#Will produce a gradient-colored triangle
 Triangle.new x1: 320, y1: 50, x2: 540, y2: 430, x3: 100, y3: 430, color: ['red', 'green', 'blue']
+
+#Going borderless?
+set borderless: true
 
 #Display the window
 show
